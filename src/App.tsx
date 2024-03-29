f2094b0789ca6d92760cb9d459524d1ed85dbc15
Steve Hunley <shunley2000@yahoo.com>
2024-03-28 19:30:02 -0500

    Game, Header and Board

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Board from './components/Board'
 
 function App() {
   return (
     <>
-      <div>
-        <h1>Hello World!</h1>
+      <div className='game'>
+        <Board />
       </div>
     </>
   )
---
+++ src/components/Game.tsx
@@ -0,0 +1,13 @@
+import Board from './Board'
+import Header from './Header'
+
+const Game = () => {
+  return (
+    <div>
+      <Header />
+      <Board />
+    </div>
+  )
+}
+
+export default Game
